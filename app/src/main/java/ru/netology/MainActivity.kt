86b5575833357589ad7fbd14c229608200e0509d
mrIlyasov <mr.ilyasov90@yandex.ru>
2022-10-26 22:38:34 +0500

    button code optimized

--- app/src/main/java/ru/netology/MainActivity.kt
+++ app/src/main/java/ru/netology/MainActivity.kt
@@ -6,27 +6,26 @@ import android.view.View
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    var scale = Scale
-    var counter: Int = 1
+
+    var boolean: Boolean = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
     }
 
-    fun onButtonClick(View: View) {
-        val textView: TextView = findViewById(R.id.textView)
 
-        if (this.counter < 2) {
-            this.scale.scale += 1
-            textView.scaleX = textView.scaleX * this.scale.scale.toFloat()
-            textView.scaleY = textView.scaleY * this.scale.scale.toFloat()
-            this.counter += 1
+
+    fun onButtonClick2(View: View) {
+        val textView: TextView = findViewById(R.id.textView)
+        if (this.boolean==false) {
+            textView.scaleX = 1f
+            textView.scaleY = 1f
+            this.boolean = true
         } else {
-            this.scale.scale = this.scale.scale * 0.5f
-            textView.scaleX = textView.scaleX * this.scale.scale * 0.5f
-            textView.scaleY = textView.scaleY * this.scale.scale * 0.5f
-            this.counter = 1
+            textView.scaleX = 4f
+            textView.scaleY = 4f
+            this.boolean = false
         }
 
 

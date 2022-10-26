d4988b96acae6aec941f94547a40805ec2464c43
mrIlyasov <mr.ilyasov90@yandex.ru>
2022-10-26 18:09:17 +0500

    netogoy icon added russian locale add

--- app/src/main/java/ru/netology/MainActivity.kt
+++ app/src/main/java/ru/netology/MainActivity.kt
@@ -2,10 +2,28 @@ package ru.netology
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun onButtonClick(View: View) {
+        val textView: TextView = findViewById(R.id.textView)
+        textView.text = "Hello kotlin"
+        var scale: Float = 1f
+        if (scale < 10) {
+            textView.scaleX = textView.scaleX * scale
+            textView.scaleY = textView.scaleY * scale
+            scale += 1f
+        } else {
+            textView.scaleX = textView.scaleX * scale
+            textView.scaleY = textView.scaleY * scale
+            scale -= 2f
+        }
     }
 }
